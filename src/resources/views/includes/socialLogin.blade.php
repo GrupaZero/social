210ad3c645d415fad9216791d68e9a8296995f3f
Michał Strzelecki <michasstrzelecki@gmail.com>
2017-10-09 13:19:47 +0200

    We can login if there is both client_secret and client_id.

--- src/resources/views/includes/socialLogin.blade.php
+++ src/resources/views/includes/socialLogin.blade.php
@@ -1,19 +1,21 @@
-@if(config('services.facebook.client_secret') || config('services.google.client_secret') || config('services.twitter.client_secret'))
+@if(config('services.facebook.client_secret') && config('services.facebook.client_id') ||
+    config('services.google.client_secret') && config('services.google.client_id') ||
+    config('services.twitter.client_secret') && config('services.twitter.client_id'))
     <div class="separator">
         <span>@lang('common.login_with')</span>
     </div>
     <div class="text-center">
-        @if(config('services.facebook.client_secret'))
+        @if(config('services.facebook.client_secret') && config('services.facebook.client_id'))
             <a href="{{ route('socialLogin',['facebook']) }}" class="btn btn-primary connect-btn">
                 <i class="fa fa-facebook"></i> Facebook
             </a>
         @endif
-        @if(config('services.google.client_secret'))
+        @if(config('services.google.client_secret') && config('services.google.client_id'))
             <a href="{{ route('socialLogin',['google']) }}" class="btn btn-danger connect-btn">
                 <i class="fa fa-google"></i> Google
             </a>
         @endif
-        @if(config('services.twitter.client_secret'))
+        @if(config('services.twitter.client_secret') && config('services.twitter.client_id'))
             <a href="{{ route('socialLogin',['twitter']) }}" class="btn btn-info connect-btn">
                 <i class="fa fa-twitter"></i> Twitter
             </a>
